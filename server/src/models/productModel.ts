522c552c0d1f2fd88e0916fbccba19181e6fdb17
YOUSUF <yufis0844@gmail.com>
2024-02-15 14:00:41 +0300

    started product model

---
+++ server/src/models/productModel.ts
@@ -0,0 +1,28 @@
+import { InferSchemaType, Schema, model } from "mongoose"
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+  },
+})
+
+type Product = InferSchemaType<typeof productSchema>
+
+export default model<Product>("Product", productSchema)
