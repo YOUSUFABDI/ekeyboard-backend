b90a4b5df0f71762d7138402a48bc5d2586017ef
YOUSUFABDI <yufis0844@gmail.com>
2024-10-16 18:35:21 +0300

    w-on

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,30 +1,29 @@
 export type CreateProductDT = {
-  productName: string
-  productPrice: number
-  productDescription: string
-  productImage: Express.Multer.File[]
-  productLikes: number
-  productStock: number
-  categoryId: number
-}
+  productName: string;
+  productPrice: number;
+  productDescription: string;
+  productImage: Express.Multer.File[];
+  productStock: number;
+  categoryId: number;
+};
 
 export type UpdateProductDT = {
-  productName: string
-  productPrice: number
-  productDescription: string
-  productImage: string[]
-  productLikes: number
-  productStock: number
-}
+  productName: string;
+  productPrice: number;
+  productDescription: string;
+  productImage: string[];
+  productLikes: number;
+  productStock: number;
+};
 
 export type updateProductParamsDT = {
-  productId: string
-}
+  productId: string;
+};
 
 export type removeProductParamsDT = {
-  productId: string
-}
+  productId: string;
+};
 
 export type CreateCategoryDT = {
-  name: string
-}
+  name: string;
+};
